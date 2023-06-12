59303319303d29fa3e051424b6481a48ff43236c
MaMWind <mauricewind@gmail.com>
2023-06-12 08:41:16 +0200

    Various changes

--- IssueTracker/Data/SeedData.cs
+++ IssueTracker/Data/SeedData.cs
@@ -1,4 +1,5 @@
 ﻿using IssueTracker.Authorization;
+using IssueTracker.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +15,42 @@ namespace IssueTracker.Data {
                 await EnsureRole(serviceProvider, adminUid, AuthorizationConstants.AdminRole);
                 await EnsureRole(serviceProvider, null, AuthorizationConstants.SupportRole);
                 await EnsureRole(serviceProvider, null, AuthorizationConstants.DeveloperRole);
+                InitializeTestData(context, adminUid);
             }
+        }
+
+        private static void InitializeTestData(ApplicationDbContext context, string adminUid) {
+            Project project = new Project() { CreatorID = adminUid, Name = "TestProjekt" };
+            Project project2 = new Project() { CreatorID = adminUid, Name = "IssueTracker" };
+            context.Project.Add(project);
+            context.Project.Add(project2);
+            context.SaveChanges();
+            var projectID1 = context.Project.First(x => x.Name == "TestProjekt").ProjectID;
+            var projectID2 = context.Project.First(x => x.Name == "IssueTracker").ProjectID;
+            Issue issue1 = new Issue() { CreatorID = adminUid, ProjectID = projectID1, Description = "This is a test issue with mockup data", Title = "Test Issue 1", Status = Status.Open, Type = Models.Type.Enhancement };
+            Issue issue2 = new Issue() { CreatorID = adminUid, ProjectID = projectID1, Description = "This is a test issue with mockup data", Title = "Test Issue 2", Status = Status.Closed, Type = Models.Type.Enhancement };
+            Issue issue3 = new Issue() { CreatorID = adminUid, ProjectID = projectID1, Description = "This is a test issue with mockup data", Title = "Test Issue 3", Status = Status.Rejected, Type = Models.Type.Enhancement };
+            Issue issue4 = new Issue() { CreatorID = adminUid, ProjectID = projectID1, Description = "This is a test issue with mockup data", Title = "Test Issue 4", Status = Status.Open, Type = Models.Type.Defect };
+            Issue issue5 = new Issue() { CreatorID = adminUid, ProjectID = projectID1, Description = "This is a test issue with mockup data", Title = "Test Issue 5", Status = Status.Open, Type = Models.Type.Task };
+            Issue issue6 = new Issue() { CreatorID = adminUid, ProjectID = projectID2, Description = "This is a test issue with mockup data", Title = "Test Issue 6", Status = Status.Open, Type = Models.Type.Enhancement };
+            Issue issue7 = new Issue() { CreatorID = adminUid, ProjectID = projectID2, Description = "This is a test issue with mockup data", Title = "Test Issue 7", Status = Status.Open, Type = Models.Type.Enhancement };
+            Issue issue8 = new Issue() { CreatorID = adminUid, ProjectID = projectID2, Description = "This is a test issue with mockup data", Title = "Test Issue 8", Status = Status.Closed, Type = Models.Type.Task };
+            Issue issue9 = new Issue() { CreatorID = adminUid, ProjectID = projectID2, Description = "This is a test issue with mockup data", Title = "Test Issue 9", Status = Status.Open, Type = Models.Type.Enhancement };
+            Issue issue10 = new Issue() { CreatorID = adminUid, ProjectID = projectID2, Description = "This is a test issue with mockup data", Title = "Test Issue 10", Status = Status.Rejected, Type = Models.Type.Defect};
+            Issue issue11 = new Issue() { CreatorID = adminUid, ProjectID = projectID2, Description = "This is a test issue with mockup data", Title = "Test Issue 11", Status = Status.Open, Type = Models.Type.Enhancement };
+            context.Issues.Add(issue1);
+            context.Issues.Add(issue2);
+            context.Issues.Add(issue3);
+            context.Issues.Add(issue4);
+            context.Issues.Add(issue5);
+            context.Issues.Add(issue6);
+            context.Issues.Add(issue7);
+            context.Issues.Add(issue8);
+            context.Issues.Add(issue9);
+            context.Issues.Add(issue11);
+            context.Issues.Add(issue10);
+            context.SaveChanges();
+
         }
 
         private static async Task<string> EnsureUser(IServiceProvider serviceProvider, string userName, string initPW) {
